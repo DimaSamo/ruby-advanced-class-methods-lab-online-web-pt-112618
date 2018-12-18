ddc3355a7597cfcd6416c2bf2b2d9b171a7ba577
Dmitri Samotessov <dimasamo96@gmail.com>
2018-12-18 08:07:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,31 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name (name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name (name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find do |song|
+      song.name = name
+    end
+  end
+
   def self.all
     @@all
   end
